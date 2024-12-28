392bcc84e5920cc7716aa4a18122ab52a54b064b
takuta9622 <t.takuya9622@gmail.com>
2024-12-28 14:16:51 +0900

    fix login function

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -1,18 +1,38 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="./css/style.css">
-</head>
-<body>
-    <form method="POST" action="{{ route('login') }}">
-    @csrf
-    <input type="email" name="email" placeholder="メールアドレス" required>
-    <input type="password" name="password" placeholder="パスワード" required>
-    <button type="submit">ログイン</button>
-</form>
+@extends('layouts.app')
 
-</body>
-</html>
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/form.css') }}" />
+@endsection
+
+@section('content')
+<div>
+    <div class="form-container">
+        <h1>ログイン</h1>
+        <form method="POST" action="{{ route('login') }}" novalidate>
+    @csrf
+            <div class="form-group">
+                <div class="form-group_label-and-error">
+                    <label for="email">メールアドレス</label>
+                    @error('email')
+                    <p class="error">{{ $message }}</p>
+                    @enderror
+                </div>
+                <input id="email" type="email" name="email" value="{{ old('email') }}" required>
+            </div>
+            <div class="form-group">
+                <div class="form-group_label-and-error">
+                    <label for="password">パスワード</label>
+                    @error('password')
+                    <p class="error">{{ $message }}</p>
+                    @enderror
+                </div>
+                <input id="password" type="password" name="password" required>
+            </div>
+            <button type="submit" class="sending-form-button">ログインする</button>
+</form>
+    </div>
+    <div class="move-register">
+        <a href="{{ route('register') }}">会員登録はこちら</a>
+    </div>
+</div>
+@endsection
