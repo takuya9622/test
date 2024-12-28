d38c7798212fa8b2ed6dab83d43c55e6a99af666
takuta9622 <t.takuya9622@gmail.com>
2024-12-28 15:18:42 +0900

    add fortify and stripe setting

--- src/config/app.php
+++ src/config/app.php
@@ -156,6 +156,7 @@ return [
     */
 
     'providers' => ServiceProvider::defaultProviders()->merge([
+        Laravel\Fortify\FortifyServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -167,8 +168,8 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
-        //App\Providers\FortifyServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\FortifyServiceProvider::class,
     ])->toArray(),
 
     /*
--- src/config/fortify.php
+++ src/config/fortify.php
@@ -73,7 +73,7 @@ return [
     |
     */
 
-    'home' => '/home',
+    'home' => '/',
 
     /*
     |--------------------------------------------------------------------------
@@ -145,15 +145,6 @@ return [
 
     'features' => [
         Features::registration(),
-        Features::resetPasswords(),
-        // Features::emailVerification(),
-        Features::updateProfileInformation(),
-        Features::updatePasswords(),
-        Features::twoFactorAuthentication([
-            'confirm' => true,
-            'confirmPassword' => true,
-            // 'window' => 0,
-        ]),
+        Features::emailVerification(),
     ],
-
 ];
